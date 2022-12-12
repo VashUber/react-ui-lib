44c981f2ab9c9c9c34d3d70497ecda910af99bdd
VashUber <mrjarokvk@gmail.com>
2022-12-12 18:56:45 +0300

    feature: added callback test for button component

--- src/components/Button/Button.test.tsx
+++ src/components/Button/Button.test.tsx
@@ -1,4 +1,4 @@
-import { render } from "@testing-library/react";
+import { fireEvent, render } from "@testing-library/react";
 import { Button } from "./Button";
 
 describe("Button", () => {
@@ -7,5 +7,22 @@ describe("Button", () => {
 
     const wrapper = render(<Button>{msg}</Button>);
     expect(wrapper.getByText(msg)).toBeInTheDocument();
+  });
+
+  it("Should call callback", () => {
+    let value = 0;
+    const cb = () => {
+      value = 100;
+    };
+    const wrapper = render(
+      <Button data-testid="button" onClick={cb}>
+        click
+      </Button>
+    );
+    const btn = wrapper.getByTestId("button");
+
+    fireEvent.click(btn);
+
+    expect(value).toBe(100);
   });
 });
